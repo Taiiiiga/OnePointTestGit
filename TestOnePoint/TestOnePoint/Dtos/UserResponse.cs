a5954e1e1c99b7aa96be464286101c9328f1137f
Gaïa <TaigoFR92@gmail.com>
2025-12-19 14:12:39 +0100

    Ajout DTO UserResponse
    (pas ajouter au commit précédent par erreur)

--- TestOnePoint/TestOnePoint/Dtos/UserResponse.cs
+++ TestOnePoint/TestOnePoint/Dtos/UserResponse.cs
@@ -1,18 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace TestOnePoint.Dtos
+﻿namespace TestOnePoint.Dtos
 {
     public class UserResponse
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        // Plain password received from clients (create / update). Not mapped to DB.
-        [NotMapped]
-        public string? Password { get; set; }
 
-        // Persisted password hash. Mapped to the existing DB column "Password" to avoid schema changes.
-        [Column("Password")]
-        public string PasswordHash { get; set; } = default!;
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
     }
 }
